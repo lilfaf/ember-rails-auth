276b1972da2fa6a2b6266d13300dff032f8cb7bc
Louis Larpin <louis.larpin@gmail.com>
2014-05-22 14:18:37 +0200

    Use ActionController::Metal instead of Base

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,25 @@
-class Api::V1::BaseController < ActionController::Base
+class Api::V1::BaseController < ActionController::Metal
+
+  include AbstractController::Rendering
+
+  include ActionController::Head
+  include ActionController::Rendering
+  include ActionController::Renderers::All
+  include ActionController::ImplicitRender
+  include ActionController::ConditionalGet
+  include ActionController::Serialization
+  include ActionController::MimeResponds
+  include ActionController::StrongParameters
+
+  include ActionController::UrlFor
+  include Rails.application.routes.url_helpers
+
+  # Before callbacks should also be executed the earliest as possible, so
+  # also include them at the bottom.
+  include AbstractController::Callbacks
+
+  include Doorkeeper::Helpers::Filter
+
   doorkeeper_for :all
 
   respond_to :json
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def index
     @users = User.all
-    respond_with @users
+    render json: @users
   end
 
   def show
     @user = User.find(params[:id])
-    respond_with @user
+    render json: @user
   end
 end
